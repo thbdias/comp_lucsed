b2be5524b3f6c0f3a4bc9baf407669ace2f43649
thbdias <thiagobalbino91@gmail.com>
2015-10-31 10:04:09 -0200

    metodos setLexema() e getLexema adicionados na classe Token.java

--- CompLucsed/src/complucsed/Token.java
+++ CompLucsed/src/complucsed/Token.java
@@ -5,8 +5,30 @@
 package complucsed;
 
 public class Token {
+    
+    private String lexema;
+    private byte token;
         
     /** construtor */
-    public Token (){}//end construtor
+    public Token (){
+        lexema = "";
+        token = -1;
+    }//end construtor
+    
+    
+    /**
+     * metodo que 
+     */
+    public void setLexema (String lexema){
+        this.lexema = lexema;
+    }//end setLexema
+    
+    
+    /**
+     * metodo que retorna o lexema do Token
+     */
+    public String getLexema(){
+        return lexema;
+    }//end getLexema
     
 }//end class Token
