aaf9721641c29b5a3c7bb7f6cfb8299bebc41322
thbdias <thiagobalbino91@gmail.com>
2015-11-02 16:53:08 -0200

    edicao metodo getToken() da classe LexicalAnalyzer.java

--- CompLucsed/src/complucsed/CompLucsed.java
+++ CompLucsed/src/complucsed/CompLucsed.java
@@ -11,7 +11,17 @@ public class CompLucsed {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        //tudo aqui e teste ainda
+        //alguem chama o analisador lexico e este retorna o proximo token
+        LexicalAnalyzer lexical = new LexicalAnalyzer("D:\\compilador\\comp_lucsed\\arq_test\\test3.txt");
+        Token token = new Token();
+      
+        token = lexical.getToken();
+        
+        String teste = token.getLexema();
+        int teste1 = token.getIdToken();
+        System.out.println ("lexema = " + teste);
+        System.out.println ("id token = " + teste1);
+    }//end main
     
-}
+}//end class CompLucsed
--- CompLucsed/src/complucsed/LexicalAnalyzer.java
+++ CompLucsed/src/complucsed/LexicalAnalyzer.java
@@ -6,4 +6,49 @@ package complucsed;
 
 public class LexicalAnalyzer {
     
-}
+    private static SymbolTab symbolTab;
+    private static Token token;
+    public static Arquivo arq;
+    
+    /**
+     * construtor
+     */
+    public LexicalAnalyzer (String nomeArq){
+        symbolTab = new SymbolTab();
+        symbolTab.initialize();
+        arq = new Arquivo (nomeArq); //carrega arquivo para memoria
+    }//end construtor
+    
+    
+    /****/
+    public Token getToken (){        
+        token = new Token ();             
+        
+        String lexema = "";
+        byte idprox = 0;  //proximo simbolo a ser inserido na SymbolTab
+        int byt = arq.readByte();    
+        
+        while (byt != ' '){       
+            lexema = lexema + (char)byt;           
+            byt = arq.readByte();            
+        }//end while          
+        
+        if ( !(symbolTab.contains(lexema)) ){   //se tabela nao cotem o lexema, insere-o        
+            idprox = symbolTab.getIdProx();
+            token.setLexema(lexema);
+            token.setIdToken(idprox);
+            symbolTab.insertSimb(lexema, idprox);            
+        }
+        else { //se tabela ja contem o lexema, nada insere na tabela
+            token.setLexema(lexema);
+            token.setIdToken(symbolTab.getToken(lexema));
+        }//end if
+        
+        return token;
+    }//end getToken
+    
+}//end class LexicalAnalyzer
+
+/**
+ * esta lendo letra a letra
+ */
